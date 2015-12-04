57e4858ee857ef0b7d9ddd6219798c747b4aecc6
sablib <sablib.iak@gmail.com>
2015-12-04 16:56:20 +0800

    Initial Commit

---
+++ ZZLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZLayout
+//
+//  Created by sablib on 15/12/4.
+//  Copyright © 2015年 sablib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZLayout
+//
+//  Created by sablib on 15/12/4.
+//  Copyright © 2015年 sablib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
